971967e2766389ac79d8ddca3909b5835272204e
chuanxinwong <ggwfnyqqqfoa@163.com>
2019-02-21 17:46:22 +0800

    修改样式。

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,237 @@
-!function(n){var e={};function t(o){if(e[o])return e[o].exports;var r=e[o]={i:o,l:!1,exports:{}};return n[o].call(r.exports,r,r.exports,t),r.l=!0,r.exports}t.m=n,t.c=e,t.d=function(n,e,o){t.o(n,e)||Object.defineProperty(n,e,{enumerable:!0,get:o})},t.r=function(n){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(n,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(n,"__esModule",{value:!0})},t.t=function(n,e){if(1&e&&(n=t(n)),8&e)return n;if(4&e&&"object"==typeof n&&n&&n.__esModule)return n;var o=Object.create(null);if(t.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:n}),2&e&&"string"!=typeof n)for(var r in n)t.d(o,r,function(e){return n[e]}.bind(null,r));return o},t.n=function(n){var e=n&&n.__esModule?function(){return n.default}:function(){return n};return t.d(e,"a",e),e},t.o=function(n,e){return Object.prototype.hasOwnProperty.call(n,e)},t.p="",t(t.s=6)}([function(n,e,t){},function(n,e,t){},function(n,e,t){},function(n,e,t){},function(n,e){n.exports=function(){var n=new BMap.Map("bdmap",{enableMapClick:!1}),e=new BMap.Point(121.441112,31.22588);n.centerAndZoom(e,16),n.enableScrollWheelZoom(!0);var t=new BMap.Marker(e);n.addOverlay(t),t.setAnimation(BMAP_ANIMATION_BOUNCE);var o=new BMap.Label("嘉春753E栋8楼",{offset:new BMap.Size(20,-10)});t.setLabel(o)}},,function(n,e,t){function o(){var n=$(document).scrollTop(),e=$(window).height();$(".block").each(function(t,o){var r,i=$(o).offset().top;n+e>i+300&&(r=r||300,$(o).find(".anibegin").each(function(n,e){setTimeout(function(){$(e).addClass("aniend")},r+r*n)}))})}t(0),t(1),t(2),t(3),t(7),t(8),t(4)(),setTimeout(function(){o()},100),$(window).on("scroll",function(n){o()}),$(".mouse img").on("click",function(){$("html, body").animate({scrollTop:.9*$(window).height()+"px"})})},function(n,e,t){},function(n,e,t){}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/js/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/js/index.js":
+/*!*************************!*\
+  !*** ./src/js/index.js ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! ../style/fontfamily.less */ "./src/style/fontfamily.less");
+__webpack_require__(/*! ../style/reset.less */ "./src/style/reset.less");
+__webpack_require__(/*! ../style/header.less */ "./src/style/header.less");
+__webpack_require__(/*! ../style/footer.less */ "./src/style/footer.less");
+__webpack_require__(/*! ../style/index.less */ "./src/style/index.less");
+__webpack_require__(/*! ../style/animate.less */ "./src/style/animate.less");
+
+
+var bdmap = __webpack_require__(/*! ../modules/bdmap */ "./src/modules/bdmap.js");
+bdmap();
+
+setTimeout(function () {
+    ckpagetop();
+}, 100);
+
+$(window).on("scroll", function (e) {
+    ckpagetop();
+})
+
+function ckpagetop() {
+    var top = $(document).scrollTop();
+    var hei = $(window).height();
+    var blks = $(".block");
+    blks.each(function (index, item) {
+        var itop = $(item).offset().top;
+        if (top + hei > itop + 300) {
+            blkani(item)
+        }
+    })
+}
+
+
+function blkani(cls, step) {
+    step = step || 300;
+    var anis = $(cls).find(".anibegin");
+    anis.each(function (index, item) {
+        setTimeout(function () {
+            $(item).addClass("aniend");
+        }, step + step * index);
+    });
+}
+
+
+$(".mouse img").on("click", function () {
+    $("html, body").animate({
+        "scrollTop": $(window).height() * 0.9 + "px"
+    });
+})
+
+/***/ }),
+
+/***/ "./src/modules/bdmap.js":
+/*!******************************!*\
+  !*** ./src/modules/bdmap.js ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = function () {
+
+    var map = new BMap.Map("bdmap",{enableMapClick:false});
+    var point = new BMap.Point(121.441112,31.22588);
+	map.centerAndZoom(point, 16);  // 初始化
+    map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+    var marker = new BMap.Marker(point);  // 创建标注
+	map.addOverlay(marker);               // 将标注添加到地图中
+	marker.setAnimation(BMAP_ANIMATION_BOUNCE); //跳动的动画
+    var label = new BMap.Label("嘉春753E栋8楼",{offset:new BMap.Size(20,-10)});
+	marker.setLabel(label);
+
+}
+
+/***/ }),
+
+/***/ "./src/style/animate.less":
+/*!********************************!*\
+  !*** ./src/style/animate.less ***!
+  \********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "./src/style/fontfamily.less":
+/*!***********************************!*\
+  !*** ./src/style/fontfamily.less ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "./src/style/footer.less":
+/*!*******************************!*\
+  !*** ./src/style/footer.less ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "./src/style/header.less":
+/*!*******************************!*\
+  !*** ./src/style/header.less ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "./src/style/index.less":
+/*!******************************!*\
+  !*** ./src/style/index.less ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "./src/style/reset.less":
+/*!******************************!*\
+  !*** ./src/style/reset.less ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ })
+
+/******/ });
 //# sourceMappingURL=index.js.map
--- public/js/products.js
+++ public/js/products.js
@@ -1,2 +1,260 @@
-!function(e){var t={};function n(o){if(t[o])return t[o].exports;var r=t[o]={i:o,l:!1,exports:{}};return e[o].call(r.exports,r,r.exports,n),r.l=!0,r.exports}n.m=e,n.c=t,n.d=function(e,t,o){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:o})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var o=Object.create(null);if(n.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var r in e)n.d(o,r,function(t){return e[t]}.bind(null,r));return o},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=17)}([function(e,t,n){},function(e,t,n){},function(e,t,n){},function(e,t,n){},function(e,t){e.exports=function(){var e=new BMap.Map("bdmap",{enableMapClick:!1}),t=new BMap.Point(121.441112,31.22588);e.centerAndZoom(t,16),e.enableScrollWheelZoom(!0);var n=new BMap.Marker(t);e.addOverlay(n),n.setAnimation(BMAP_ANIMATION_BOUNCE);var o=new BMap.Label("嘉春753E栋8楼",{offset:new BMap.Size(20,-10)});n.setLabel(o)}},function(e,t,n){},,,,,,,,,,,,function(e,t,n){n(0),n(1),n(2),n(3),n(5),n(18),n(4)(),$(".apps .donwload a").hover(function(){var e=$(this),t=e.attr("href"),n=e.find(".qrcode");console.log(n),0==n.length&&(n=$('<div class="qrcode"><p>请扫码下载</p><div></div></div>'),e.append(n),new QRCode(n.find("div").get(0),{text:t,width:110,height:110,colorDark:"#000000",colorLight:"#ffffff",correctLevel:QRCode.CorrectLevel.H})),n.show(),n.animate({bottom:"50px",opacity:1},200)},function(){var e=$(this).find(".qrcode");e.animate({bottom:"0",opacity:0},200,function(){e.hide()})}),$(".cshq").hover(function(){var e=$(this).find(".qrcode");e.show(),e.animate({bottom:"50px",opacity:1},200)},function(){var e=$(this).find(".qrcode");e.animate({bottom:"0",opacity:0},200,function(){e.hide()})})},function(e,t,n){}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/js/products.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/js/products.js":
+/*!****************************!*\
+  !*** ./src/js/products.js ***!
+  \****************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! ../style/fontfamily.less */ "./src/style/fontfamily.less");
+__webpack_require__(/*! ../style/reset.less */ "./src/style/reset.less");
+__webpack_require__(/*! ../style/header.less */ "./src/style/header.less");
+__webpack_require__(/*! ../style/footer.less */ "./src/style/footer.less");
+__webpack_require__(/*! ../style/public.less */ "./src/style/public.less");
+__webpack_require__(/*! ../style/products.less */ "./src/style/products.less");
+
+
+var bdmap = __webpack_require__(/*! ../modules/bdmap */ "./src/modules/bdmap.js");
+bdmap();
+
+
+
+$(".apps .donwload a").hover(function () {
+
+    var a = $(this);
+
+    var href = a.attr("href");
+
+    var qrcode = a.find(".qrcode");
+    console.log(qrcode)
+
+    if (qrcode.length == 0) {
+        qrcode = $('<div class="qrcode"><p>请扫码下载</p><div></div></div>');
+        a.append(qrcode);
+
+        new QRCode(qrcode.find("div").get(0), {
+            text: href,
+            width: 110,
+            height: 110,
+            colorDark: "#000000",
+            colorLight: "#ffffff",
+            correctLevel: QRCode.CorrectLevel.H
+        });
+    }
+
+
+    qrcode.show();
+    qrcode.animate({
+        bottom: "50px",
+        opacity: 1,
+    }, 200);
+
+
+}, function () {
+    var qrcode = $(this).find(".qrcode");
+    qrcode.animate({
+        bottom: "0",
+        opacity: 0,
+    }, 200, function () {
+        qrcode.hide();
+    });
+})
+
+
+$(".cshq").hover(function () {
+    var qrcode = $(this).find(".qrcode");
+    qrcode.show();
+    qrcode.animate({
+        bottom: "50px",
+        opacity: 1,
+    }, 200);
+}, function () {
+    var qrcode = $(this).find(".qrcode");
+    qrcode.animate({
+        bottom: "0",
+        opacity: 0,
+    }, 200, function () {
+        qrcode.hide();
+    });
+})
+
+/***/ }),
+
+/***/ "./src/modules/bdmap.js":
+/*!******************************!*\
+  !*** ./src/modules/bdmap.js ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = function () {
+
+    var map = new BMap.Map("bdmap",{enableMapClick:false});
+    var point = new BMap.Point(121.441112,31.22588);
+	map.centerAndZoom(point, 16);  // 初始化
+    map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+    var marker = new BMap.Marker(point);  // 创建标注
+	map.addOverlay(marker);               // 将标注添加到地图中
+	marker.setAnimation(BMAP_ANIMATION_BOUNCE); //跳动的动画
+    var label = new BMap.Label("嘉春753E栋8楼",{offset:new BMap.Size(20,-10)});
+	marker.setLabel(label);
+
+}
+
+/***/ }),
+
+/***/ "./src/style/fontfamily.less":
+/*!***********************************!*\
+  !*** ./src/style/fontfamily.less ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "./src/style/footer.less":
+/*!*******************************!*\
+  !*** ./src/style/footer.less ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "./src/style/header.less":
+/*!*******************************!*\
+  !*** ./src/style/header.less ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "./src/style/products.less":
+/*!*********************************!*\
+  !*** ./src/style/products.less ***!
+  \*********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "./src/style/public.less":
+/*!*******************************!*\
+  !*** ./src/style/public.less ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "./src/style/reset.less":
+/*!******************************!*\
+  !*** ./src/style/reset.less ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ })
+
+/******/ });
 //# sourceMappingURL=products.js.map
